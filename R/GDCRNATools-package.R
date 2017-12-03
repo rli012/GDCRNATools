f662018e402a76960162c9ec678678e19ae484c0
Jialab-UCR <jialab.ucr@gmail.com>
2017-12-02 19:24:54 -0800

    Add files via upload

---
+++ R/GDCRNATools-package.R
@@ -0,0 +1,22 @@
+#' This is an easy-to-use package for downloading, organizing, and integrative analyzing RNA expression 
+#' data in GDC with an emphasis on deciphering the lncRNA-mRNA related ceRNA regulatory network in cancer.
+#'
+#' @name GDCRNATools-package
+#' @aliases GDCRNATools-package GDCRNATools
+#' @docType package
+NULL
+
+
+
+#' Output of \code{\link{gdcDEAnalysis}} for downstream analysis
+#' @name DEGAll
+#' @docType data
+#' @keywords datasets
+NULL
+
+
+#' Output of \code{\link{gdcEnrichAnalysis}} for visualization
+#' @name enrichOutput
+#' @docType data
+#' @keywords datasets
+NULL
