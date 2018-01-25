a5f820a5cd29e3a15e9c3bca510ce29b8ca9d025
Jialab-UCR <jialab.ucr@gmail.com>
2018-01-25 14:19:21 -0800

    message

--- R/gdcCEAnalysis.R
+++ R/gdcCEAnalysis.R
@@ -75,18 +75,17 @@ gdcCEAnalysis <- function(lnc, pc, deMIR=NULL, lnc.targets='starBase',
     
     hyperOutput <- hyperTestFun(lnc, pc, deMIR, 
         lnc.targets=lnc.targets, pc.targets=pc.targets)
-    message ('Step 1/3: Hypergenometric test done !\n')
+    message ('Step 1/3: Hypergenometric test done !')
     
     regOutput <- multiRegTestFun(hyperOutput, rna.expr=rna.expr, 
         mir.expr=mir.expr)
     message ('Step 2/3: Correlation analysis done !\n',
-        'Step 3/3: Regulation pattern analysis done !\n')
+        'Step 3/3: Regulation pattern analysis done !')
     
     ceOutput <- data.frame(hyperOutput, regOutput, row.names=NULL)
     
     return(ceOutput)
 }
-
 
 
 #### hypergeometric test
